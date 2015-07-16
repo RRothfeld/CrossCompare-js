0e4103fdbd26cc7cd6932e3a41b1132fd8fe60d5
RRothfeld <raoul.rothfeld+github@gmail.com>
2015-07-16 22:59:27 +0100

    multi compare

--- public/js/example.js
+++ public/js/example.js
@@ -1,15 +1,16 @@
-// Define charts
-var movementsChart = dc.lineChart('#movements-chart'),
-		movementsTimeChart = dc.barChart('#movements-time-chart'),
-		delayChart = dc.barChart('#delay-length-chart');
-
 // Load data from csv file
 //d3.csv("/data/flightsDec08.csv", function(data) {
 d3.csv("/data/MINI.csv", function(data) {
 
+	// Define charts
+	var movementsChart = dc.lineChart('#movements-chart'),
+			movementsTimeChart = dc.barChart('#movements-time-chart'),
+			delayChart = dc.barChart('#delay-length-chart');
+
 	// Parse dates and times from .csv
 	var dateFormat = d3.time.format('%d-%m-%Y'),
 			timeFormat = d3.time.format('%H:%M');
+
 	data.forEach(function (d) {
 		d.DDMMYYYY = dateFormat.parse(d.DDMMYYYY);
 		d.ArrTimeRounded = timeFormat.parse(d.ArrTimeRounded);
@@ -47,69 +48,74 @@ d3.csv("/data/MINI.csv", function(data) {
 	.dimension(flights)
 	.group(all)
 	.html({
-		some:'<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records' +
-			' | <a href=\'javascript:dc.filterAll(); dc.renderAll();\'\'>Reset All</a>',
+		some:'<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records' + ' | <a href=\'javascript:dc.filterAll(); dc.renderAll();\'\'>Reset All</a>',
 		all:'All records selected. Please click on the graph to apply filters.'
 	});
 
+	// Define charts properties
+	movementsChart
+	.renderArea(true)
+	.height(300)
+	.margins({top: 10, right: 50, bottom: 30, left: 50})
+	.dimension(date)
+	.group(byDate)
+	.elasticY(true)
+	.x(d3.time.scale().domain([minDate, maxDate]))
+	.renderHorizontalGridLines(true)
+	.xUnits(d3.time.days)
+	.mouseZoomable(true)
+	.yAxis().ticks(7);
+
+	movementsTimeChart
+	.height(40)
+	.margins({top: 0, right: 50, bottom: 20, left: 50})
+	.dimension(date)
+	.group(byDate)
+	.centerBar(true)
+	.elasticY(true)
+	.x(d3.time.scale().domain([minDate, maxDate]))
+	.xUnits(d3.time.days)
+	.gap(1)
+	.yAxis().ticks(0);
+
+	delayChart
+	.height(250)
+	.margins({top: 10, right: 50, bottom: 30, left: 50})
+	.dimension(delay)
+	.group(byDelay)
+	.gap(1)
+	.elasticY(true)
+	.x(d3.scale.linear().domain([minDelay, maxDelay]))
+	.renderHorizontalGridLines(true)
+	.mouseZoomable(false)
+	.yAxis().ticks(7);
+
+	// Update chart widths
 	renderCharts = function () {
-		// Retrieve available space for charts
+		// Retrieve available space for charts via DOM
 		var movementsChartWidth = $('#movements-chart-width').width(),
 				destinationChartWidth = $('#destinations-chart-width').width(),
 				delayChartWidth = $('#delay-length-chart-width').width();
 
-		// Define charts properties
-		movementsChart
-		.renderArea(true)
-		.width(movementsChartWidth)
-		.height(300)
-		.margins({top: 10, right: 50, bottom: 30, left: 50})
-		.dimension(date)
-		.group(byDate)
-		.elasticY(true)
-		.x(d3.time.scale().domain([minDate, maxDate]))
-		.renderHorizontalGridLines(true)
-		.xUnits(d3.time.days)
-		.mouseZoomable(true)
-		.yAxis().ticks(7);
+		// Set chart widths
+		movementsChart.width(movementsChartWidth);
+		movementsTimeChart.width(movementsChartWidth)
+		delayChart.width(delayChartWidth);
 
-		movementsTimeChart
-		.width(movementsChartWidth)
-		.height(40)
-		.margins({top: 0, right: 50, bottom: 20, left: 50})
-		.dimension(date)
-		.group(byDate)
-		.centerBar(true)
-		.elasticY(true)
-		.x(d3.time.scale().domain([minDate, maxDate]))
-		.xUnits(d3.time.days)
-		.gap(1)
-		.yAxis().ticks(0);
-
-		delayChart
-		.width(delayChartWidth)
-		.height(250)
-		.margins({top: 10, right: 50, bottom: 30, left: 50})
-		.dimension(delay)
-		.group(byDelay)
-		.gap(1)
-		.elasticY(true)
-		.x(d3.scale.linear().domain([minDelay, maxDelay]))
-		.renderHorizontalGridLines(true)
-		.mouseZoomable(false)
-		.yAxis().ticks(7);
-
-		// Render all charts
+		// Render all charts (update)
 		dc.renderAll();
 	};
 
 	// Render charts upon page load
 	renderCharts();
 
+
+	// JQUERY
+
 	// Render charts upon page resize
 	$(window).resize(function() {
 		dc.filterAll(); // Reset filters as filters are not re-sizable
-		renderCharts();
+		renderCharts(); // Re-render charts
 	});
 
 	// Airport selection menu
@@ -119,10 +125,14 @@ d3.csv("/data/MINI.csv", function(data) {
 	});
 
 	var compChart, byDate2, byDate, date;
-
+	var compareActive = false;
+	var subCharts = [];
+	var colors = ['orange','red','green','yellow','blue','brown'];
+	var i = 0;
 	// Graph test
 	$('#test').on('click', function() {
-		if (compChart == null) {
+		if (!compareActive) {
+			compareActive = true;
 
 			compChart = dc.compositeChart('#comparison-chart');
 
@@ -142,6 +152,9 @@ d3.csv("/data/MINI.csv", function(data) {
 			// Retrieve available space for charts
 			var compChartWidth = $('#comparison-chart-width').width();
 
+			subCharts = [];
+			subCharts.push(dc.lineChart(compChart).group(byDate));
+
 			// Define charts properties
 			compChart
 			.width(compChartWidth)
@@ -152,29 +165,29 @@ d3.csv("/data/MINI.csv", function(data) {
 			.mouseZoomable(false)
 			.brushOn(false)
 			.elasticY(true)
-			.compose([
-				dc.lineChart(compChart).group(byDate)
-			]);
+			.compose(subCharts);
 
 			// Render all charts
-			dc.renderAll();
+			compChart.render();
 		} else {
 			var test23 = crossfilter(airport.top(Infinity));
 			date2 = test23.dimension(function(d) { return d.DDMMYYYY; });
 			byDate2 = date2.group();
 
-			compChart
-			.compose([
-				dc.lineChart(compChart).group(byDate),
-				dc.lineChart(compChart).group(byDate2).colors('orange')
-			]);
-			dc.redrawAll();
+			subCharts.push(dc.lineChart(compChart).group(byDate2).colors(colors[i++]));
+			i = i % colors.length;
+		 	compChart
+		 	.compose(subCharts);
+
+			compChart.redraw();
 		}
 	});
 
-	$('#test2').on('click', function() {
-		compChart.height(1);
-		dc.renderAll();
-		compChart = null;
+	$('#reset-compare').on('click', function() {
+		subCharts = [];
+		compChart.compose(subCharts);
+		compChart.render();
+
+		compareActive = false;	
 	});
 });
