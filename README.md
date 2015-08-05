e8f58499f27bb61919c4d55b2f8b92c8eb5d4357
RRothfeld <raoul.rothfeld+github@gmail.com>
2015-08-05 12:38:47 +0100

    finished site itself

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ CrossCompare started as a Master of Science project for Software Development at 
 
 Please feel free to utilise, explore, enhance, or give feedback. Special thank's to the authors of [jQuery](https://jquery.com/), [d3.js](http://d3js.org/), [crossfilter](http://square.github.io/crossfilter/), [dc.js](https://dc-js.github.io/dc.js/), [c3.js](http://c3js.org/), [AdminLTE](https://almsaeedstudio.com/AdminLTE), [bootstrap](http://getbootstrap.com/), [smoothscroll](https://github.com/cferdinandi/smooth-scroll), and [popup overlay](http://dev.vast.com/jquery-popup-overlay/)!
 
-# Usage
+# CrossCompare Usage
 [Download](http://rrothfeld.github.io/CrossCompare-js/download/crosscompare.zip) the crosscompare zip-file and extract it into your web page folder. Include the JavaScript-file within your webpage. Set up and customize CrossCompare for your usage according to the [API](http://rrothfeld.github.io/CrossCompare-js/docs.html) or the [tutorial](http://rrothfeld.github.io/CrossCompare-js/tutorial.html) - a great starting place for a new CrossCompare application.
 
-# Installation
+# Installation (replica web site)
 This repository holds all code for the web site shown under: http://rrothfeld.github.io/CrossCompare-js/. 
 
  1. Have [Node.js](https://nodejs.org/) and [git](https://git-scm.com/) installed
@@ -16,11 +16,11 @@ This repository holds all code for the web site shown under: http://rrothfeld.gi
  3. Cd into the created folder: `cd CrossCompare-js`
  4. Install node modules: `npm install`
  5. Run the application: `npm start`
- 6. Open http://localhost:3000/ in your web browser
+ 6. Open [http://localhost:3000/](http://localhost:3000/) in your web browser
 
 A possible error upon executing `npm start` may be the result of the default port `3000` being occupied by another service. If so, simply change line 11 in app.js to another port number and run `npm start` again.
 
-# Testing
+# CrossCompare Testing
 Unit testing of CrossCompare's functions is done using [mocha](http://mochajs.org/) with the help of [chai assertion library](http://chaijs.com/).
 
 To run the testing code, simply run execute  `./node_modules/.bin/mocha` from within the CrossCompare-js folder.
--- test/test.js
+++ test/test.js
@@ -1,35 +1,30 @@
 var chai = require('chai'),
 		cc = require('../crosscompare');
 
-var pos = function() { return Math.floor(Math.random() * 999) + 1; };
-var neg = function() { return -pos(); };
-var txt = function() { return 'abc' + pos(); };
+// Generate random, whole number between 0 and 1000
+function pos() { return Math.floor(Math.random() * 1000); };
+// Generate random, whole number between -1000 and 0
+function neg() { return -pos(); };
+// Generate semi-random string
+function txt() { return 'abc' + pos(); };
 
-describe('crosscompare', function() {
+// Test CrossCompare.js
+describe('CrossCompare.js', function() {
 
 	it('should be defined', function () {
 		chai.assert.isDefined(cc);
 	});
 
-	describe('default values', function () {
+	describe('Required default values', function () {
 		it('should have a default height', function () {
 			chai.assert.isAbove(cc.height, 0);
 		});
 		it('should have a default anchor', function () {
 			chai.assert.isString(cc.anchor);
 		});
-		it('should have a default active flag', function () {
-			chai.assert.isBoolean(cc.active);
-		});
-		it('should have no chart by default', function () {
-			chai.expect(cc.chart).to.be.empty;
-		});
-		it('should have no added charts by default', function () {
-			chai.expect(cc.charts).to.be.empty;
-		});
 	});
 
-	describe('.setHeight()', function () {
+	describe('Function .setHeight()', function () {
 		it('should accept positive integers', function () {
 			var number = pos();
 			cc.setHeight(number);
@@ -47,7 +42,7 @@ describe('crosscompare', function() {
 		});
 		it('should ignore non-integers', function () {
 			var unchanged = cc.height;
-			cc.setHeight('test');
+			cc.setHeight(txt());
 			chai.assert.equal(unchanged, cc.height);
 		});
 		it('should ignore no parameter', function () {
@@ -60,10 +55,98 @@ describe('crosscompare', function() {
 			chai.assert.equal(cc, cc.setHeight(pos()));
 			chai.assert.equal(cc, cc.setHeight(0));
 			chai.assert.equal(cc, cc.setHeight(neg()));
+			chai.assert.equal(cc, cc.setHeight(200));
 		});
 	});
 
-	describe('.setAnchor()', function () {
+	describe('Function .setWidth()', function () {
+		it('should accept positive integers', function () {
+			var number = pos();
+			cc.setWidth(number);
+			chai.assert.equal(number, cc.width);
+		});
+		it('should accept zero', function () {
+			cc.setWidth(0);
+			chai.assert.equal(0, cc.width);
+		});
+		it('should accept \'auto\'', function () {
+			cc.setWidth('auto');
+			chai.assert.equal('auto', cc.width);
+		});
+		it('should ignore negative integers', function () {
+			var unchanged = cc.width;
+			var number = neg();
+			cc.setWidth(number);
+			chai.assert.equal(unchanged, cc.width);
+		});
+		it('should ignore non-\'auto\' strings', function () {
+			var unchanged = cc.width;
+			cc.setWidth(txt());
+			chai.assert.equal(unchanged, cc.width);
+		});
+		it('should ignore no parameter', function () {
+			var unchanged = cc.width;
+			cc.setWidth();
+			chai.assert.equal(unchanged, cc.width);
+		});
+		it('should be chain-able', function () {
+			chai.assert.equal(cc, cc.setWidth());
+			chai.assert.equal(cc, cc.setWidth(pos()));
+			chai.assert.equal(cc, cc.setWidth(0));
+			chai.assert.equal(cc, cc.setWidth(neg()));
+			chai.assert.equal(cc, cc.setWidth(200));
+		});
+	});
+
+	describe('Function .setPadding()', function () {
+		it('should accept positive integers', function () {
+			var a = pos(),
+					b = pos(),
+					c = pos(),
+					d = pos();
+			cc.setPadding(a, b, c, d);
+			chai.assert.equal(a, cc.padding.top);
+			chai.assert.equal(b, cc.padding.right);
+			chai.assert.equal(c, cc.padding.bottom);
+			chai.assert.equal(d, cc.padding.left);
+		});
+		it('should accept zero', function () {
+			cc.setPadding(0, 0, 0, 0);
+			chai.assert.equal(0, cc.padding.top);
+			chai.assert.equal(0, cc.padding.right);
+			chai.assert.equal(0, cc.padding.bottom);
+			chai.assert.equal(0, cc.padding.left);
+		});
+		it('should accept negative integers', function () {
+			var a = neg(),
+					b = neg(),
+					c = neg(),
+					d = neg();
+			cc.setPadding(a, b, c, d);
+			chai.assert.equal(a, cc.padding.top);
+			chai.assert.equal(b, cc.padding.right);
+			chai.assert.equal(c, cc.padding.bottom);
+			chai.assert.equal(d, cc.padding.left);
+		});
+		it('should ignore non-integers', function () {
+			var unchanged = cc.padding;
+			cc.setPadding(txt(),neg(),pos(),txt());
+			chai.assert.equal(unchanged, cc.padding);
+		});
+		it('should ignore no parameter', function () {
+			var unchanged = cc.padding;
+			cc.setPadding();
+			chai.assert.equal(unchanged, cc.padding);
+		});
+		it('should be chain-able', function () {
+			chai.assert.equal(cc, cc.setPadding());
+			chai.assert.equal(cc, cc.setPadding(pos(), pos(), pos(), pos()));
+			chai.assert.equal(cc, cc.setPadding(neg(), neg(), neg(), neg()));
+			chai.assert.equal(cc, cc.setPadding(0, 0, 0, 0));
+		});
+	});
+
+	describe('Function .setAnchor()', function () {
 		it('should accept String with 1 or more characters', function () {
 			var text = txt();
 			cc.setAnchor(text);
@@ -86,7 +169,148 @@ describe('crosscompare', function() {
 		it('should be chain-able', function () {
 			chai.assert.equal(cc, cc.setAnchor());
 			chai.assert.equal(cc, cc.setAnchor(''));
-			chai.assert.equal(cc, cc.setAnchor('test'));
+			chai.assert.equal(cc, cc.setAnchor(txt()));
+		});
+	});
+
+	describe('Function .setDateFormat()', function () {
+		it('should accept String with 1 or more characters', function () {
+			var text = txt();
+			cc.setDateFormat(text);
+			chai.assert.equal(text, cc.dateFormat);
+
+			var character = 'a';
+			cc.setDateFormat(character);
+			chai.assert.equal(character, cc.dateFormat);
+		});
+		it('should ignore empty Strings', function () {
+			var unchanged = cc.dateFormat;
+			cc.setDateFormat('');
+			chai.assert.equal(unchanged, cc.dateFormat);
+		});
+		it('should ignore no parameter', function () {
+			var unchanged = cc.dateFormat;
+			cc.setDateFormat();
+			chai.assert.equal(unchanged, cc.dateFormat);
+		});
+		it('should be chain-able', function () {
+			chai.assert.equal(cc, cc.setDateFormat());
+			chai.assert.equal(cc, cc.setDateFormat(''));
+			chai.assert.equal(cc, cc.setDateFormat(txt()));
+		});
+	});
+
+	describe('Function .setGrid()', function () {
+		it('should accept two booleans', function () {
+			cc.setGrid(true, true);
+			chai.assert.isTrue(cc.xGrid);
+			chai.assert.isTrue(cc.yGrid);
+
+			cc.setGrid(false, false);
+			chai.assert.isFalse(cc.xGrid);
+			chai.assert.isFalse(cc.yGrid);
+		});
+		it('should ignore one boolean', function () {
+			var unchangedX = cc.xGrid;
+			var unchangedY = cc.yGrid;
+			cc.setGrid(false);
+			chai.assert.equal(unchangedX, cc.xGrid);
+			chai.assert.equal(unchangedX, cc.yGrid);
+			cc.setGrid(true);
+			chai.assert.equal(unchangedX, cc.xGrid);
+			chai.assert.equal(unchangedX, cc.yGrid);
+		});
+		it('should ignore no parameter', function () {
+			var unchangedX = cc.xGrid;
+			var unchangedY = cc.yGrid;
+			cc.setGrid();
+			chai.assert.equal(unchangedX, cc.xGrid);
+			chai.assert.equal(unchangedX, cc.yGrid);
+		});
+		it('should ignore non-boolean parameters', function () {
+			var unchangedX = cc.xGrid;
+			var unchangedY = cc.yGrid;
+			cc.setGrid(pos(), txt());
+			chai.assert.equal(unchangedX, cc.xGrid);
+			chai.assert.equal(unchangedX, cc.yGrid);
+		});
+		it('should be chain-able', function () {
+			chai.assert.equal(cc, cc.setGrid(pos(), pos()));
+			chai.assert.equal(cc, cc.setGrid());
+			chai.assert.equal(cc, cc.setGrid(false, false));
+		});
+	});
+
+	describe('Function .setFlash()', function () {
+		it('should accept boolean', function () {
+			cc.setFlash(true);
+			chai.assert.isTrue(cc.flash);
+
+			cc.setFlash(false);
+			chai.assert.isFalse(cc.flash);
+		});
+		it('should ignore no parameter', function () {
+			var unchanged = cc.flash;
+			cc.setFlash();
+			chai.assert.equal(unchanged, cc.flash);
+		});
+		it('should ignore non-boolean parameter', function () {
+			var unchanged = cc.flash;
+			cc.setFlash(pos());
+			cc.setFlash(txt());
+			chai.assert.equal(unchanged, cc.flash);
+		});
+		it('should be chain-able', function () {
+			chai.assert.equal(cc, cc.setFlash());
+			chai.assert.equal(cc, cc.setFlash(true));
+		});
+	});
+
+	describe('Function .addLegend()', function () {
+		it('should accept object with title', function () {
+			chai.assert.equal(0, cc.legends.length);
+			cc.addLegend(txt(), txt());
+			chai.assert.equal(1, cc.legends.length);
+		});
+		it('should accept object without title', function () {
+			var previous = cc.legends.length;
+			cc.addLegend(txt());
+			chai.assert.equal(previous + 1, cc.legends.length);
+		});
+		it('should ignore no parameters', function () {
+			var previous = cc.legends.length;
+			cc.addLegend();
+			chai.assert.equal(previous, cc.legends.length);
+		});
+		it('should be chain-able', function () {
+			chai.assert.equal(cc, cc.addLegend());
+			chai.assert.equal(cc, cc.addLegend(txt(), txt()));
+			chai.assert.equal(cc, cc.addLegend(txt()));
+		});
+	});
+
+	describe('Function .setOverwrite()', function () {
+		it('should accept boolean', function () {
+			cc.setOverwrite(true);
+			chai.assert.isTrue(cc.overwrite);
+
+			cc.setOverwrite(false);
+			chai.assert.isFalse(cc.overwrite);
+		});
+		it('should ignore no parameter', function () {
+			var unchanged = cc.overwrite;
+			cc.setOverwrite();
+			chai.assert.equal(unchanged, cc.overwrite);
+		});
+		it('should ignore non-boolean parameter', function () {
+			var unchanged = cc.overwrite;
+			cc.setOverwrite(pos());
+			cc.setOverwrite(txt());
+			chai.assert.equal(unchanged, cc.overwrite);
+		});
+		it('should be chain-able', function () {
+			chai.assert.equal(cc, cc.setOverwrite());
+			chai.assert.equal(cc, cc.setOverwrite(false));
 		});
 	});
 });
