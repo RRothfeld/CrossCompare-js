560a9f58ab30089f3bd62e009e14788896c4e8ea
RRothfeld <raoul.rothfeld+github@gmail.com>
2015-08-05 22:54:00 +0100

    aws preparation

--- public/js/experiment-live.js
+++ public/js/experiment-live.js
@@ -11,31 +11,31 @@ function log(item) {
 
 function loop() {
 	if ($(document).find('iframe').length > 0) {
-		$(document).find('iframe').on('load', function () {
-			log('survey-next');
+		setTimeout(function(){ 
+			$(document).find('iframe').on('load', function () {
+				log('survey-next');
 
-			dc.filterAll();
-			dc.redrawAll();
+				dc.filterAll();
+				dc.redrawAll();
 
-			if (page == 0)
-				$('#overlay').fadeOut('fast');
+				if (page == 0)
+					$('#overlay').fadeOut('fast');
+				else if (page == 1) {
+					$('.openCross').show();
+					crossActive = true;
+				} else if (page == 2) {
+					$('#overlay').fadeIn('fast');
+					setTimeout(function(){ window.location = '/experiment'; }, 3000);
+				}
 
-			if (page > 0) {
-				$('.openCross').show();
-				crossActive = true;
-			}
-
-			if (page >= 2) {
-				$('#overlay').fadeIn('fast');
-			}
-
-			page++;
-		});
+				page++;
+			});
+		}, 3000);
 	} else
-		setTimeout(function(){ loop(); }, 2000);
+		setTimeout(function(){ loop(); }, 1000);
 };
 
-setTimeout(function(){ loop(); }, 2000);
+setTimeout(function(){ loop(); }, 1000);
 
 $('button').on('click', function() { log(this.outerHTML); });
 
